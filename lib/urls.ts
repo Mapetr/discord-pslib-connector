04d8a90f36d7653a50e4d2c6bc45db8247d00c83
Mapetr <info@mapetr.moe>
2024-09-01 13:33:56 +0200

    use url from discord-api-types

--- lib/urls.ts
+++ lib/urls.ts
@@ -1,3 +1,5 @@
+import {OAuth2Routes} from "discord-api-types/v10";
+
 export function getMicrosoftLoginURL(origin: string): string {
   const tenant = process.env.MSAL_TENANT_ID ?? "";
   const client_id = process.env.MSAL_CLIENT_ID ?? "";
@@ -9,5 +11,5 @@ export function getMicrosoftLoginURL(origin: string): string {
 export function getDiscordLoginURL(origin: string): string {
   const client_id = process.env.DISCORD_CLIENT_ID ?? "";
   if (!client_id) console.error("Missing DISCORD_CLIENT_ID");
-  return `https://discord.com/oauth2/authorize?response_type=code&client_id=${client_id}&scope=identify%20guild.join&redirect_uri=${encodeURIComponent(origin)}%2Fdiscord&prompt=consent`
+  return `${OAuth2Routes.authorizationURL}?response_type=code&client_id=${client_id}&scope=identify%20guild.join&redirect_uri=${encodeURIComponent(origin)}%2Fdiscord&prompt=consent`
 }
