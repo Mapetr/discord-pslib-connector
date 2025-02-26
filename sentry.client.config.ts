53d0d0fac7a11d98c7efb64c1c11b3bc65e3f676
Mapetr <info@mapetr.moe>
2025-02-26 09:11:19 +0100

    replace dsn

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://9289b77da762471f8466fd2fb52f9519@glitch.ayai.dev/1",
+  dsn: "https://566a09b00cf8faeaacd2e1db9d3f5f42@o4505744251224064.ingest.us.sentry.io/4505795657662464",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
